a3489899476e5f0927beb1d08696a208310dd780
Tim Schäfer <ts+code@rcmd.org>
2017-11-10 16:52:30 +0100

    event visualisation as span, 1st throw

--- src/Timeline.js
+++ src/Timeline.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 export type EventData = {
-	+startIndex: number,
+	+start: number,
 	+duration: number,
     +eventTitle: string,
 	+eventColor?: string
@@ -9,15 +9,33 @@ export type EventData = {
 
 export type SingleTimelineData = {
     +events: Array<EventData>,
-    +timelineTitle: string
+    +timelineTitle: string,
 }
 
 export class Timeline extends Component {
     props: SingleTimelineData;
+	
+	const eventVisualisations = this.props.events.map((event: EventData) => {
+		const widthString = event.duration + 'px';
+		const eventColorString = this.props.eventColor ? this.props.eventColor : 'gray';
+		const eventStyle = {
+			'width': widthString,
+			'backgroundColor': eventColorString
+		};
+		const eventText = "[" + event.eventTitle + ": " + event.duration + "]";
+		const vis = <span className="event" style={eventStyle}>{eventText}</span>;
+		return vis; 
+	});
     
     render() {
         return(
-            <div className="Timeline">{this.props.timelineTitle}</div>  
+            <div className="timeline">
+			    <div className="timeline-title">
+				    {this.props.timelineTitle}
+				</div>
+				<div className="timeline-visualization">
+				</div>
+			</div>  
         );
     }
 }
