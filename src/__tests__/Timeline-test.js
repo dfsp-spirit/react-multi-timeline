98752f5c706ae51ead515be84e77b3d356fd1a07
Tim Schäfer <ts+code@rcmd.org>
2017-12-27 10:54:33 +0100

    Add more unit tests for a simple timeline with a single event that starts after time point zero.

--- src/__tests__/Timeline-test.js
+++ src/__tests__/Timeline-test.js
@@ -1,20 +1,55 @@
 import React from 'react';
-import {shallow, render } from 'enzyme';
+import {shallow, render, mount } from 'enzyme';
 import { Timeline } from '../Timeline';
 
-test('Timline shows the title', () => {
-  
-  
-  const timeline = render(<Timeline
-            timelineTitle="Timeline"
-            events={[{eventTitle: 'evenet1', start: 5, duration: 10}]}
-            displayTimeUnits={true}
-            timeUnitLabel="months"
-            useParentWidth={true}
-            lastEventEndAbsoluteOverAllTimelines={10}
-          />);
-		  
-    const len = timeline.find('div').length;
+describe('Test timeline with a single event that starts after time point zero', function () {
 
-  expect(len).toEqual(2);
+  let componentInstance;
+
+  beforeEach(function () {
+    const component = <Timeline
+    timelineTitle="Timeline"
+    events={[{eventTitle: 'event1', start: 5, duration: 10}]}
+    displayTimeUnits={true}
+    timeUnitLabel="months"
+    useParentWidth={true}
+    lastEventEndAbsoluteOverAllTimelines={10}
+    />;
+    componentInstance = mount(component);
+  });
+
+
+  const countDivs = function () {
+    return componentInstance.find('div').length;
+  };
+
+  const countEvents = function () {
+    return componentInstance.find('span.event').length;
+  };
+
+  const countTimeBetweenEvents = function () {
+    return componentInstance.find('span.timeBetweenEvents').length;
+  };
+
+  const countEventLikes = function () {
+    return componentInstance.find('span.event-like').length;
+  };
+  
+  it('Contains exactly four divs', function () {
+    expect(countDivs()).toBe(4);
+  });
+
+  it('Contains exactly one event', function () {
+    expect(countEvents()).toBe(1);
+  });
+
+  it('Contains exactly one empty time period between events', function () {
+    expect(countTimeBetweenEvents()).toBe(1);
+  });
+
+  it('Contains exactly two event-like entries', function () {
+    expect(countEventLikes()).toBe(2);
+  });
+
+
 });
